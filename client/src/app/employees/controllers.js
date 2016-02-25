8ea23b9864096fe754fb0b0f9d4c3081f2639a78
Emily Stinger <emily.stinger@objectpartners.com>
2016-02-25 14:36:53 -0600

    Remove var vm reference and let the lab code snippet include it.

--- client/src/app/employees/controllers.js
+++ client/src/app/employees/controllers.js
@@ -2,12 +2,12 @@ angular.module('app.employees.controllers', [])
   
   .controller('EmployeeCtrl', 
     function (data) {
-      var vm = this;
 
-      // TODO : implement a function on controller (vm) to request the list of employees
-      // TODO : implement a function on controller (vm) to remove an employee
-      // TODO : implement a function on controller (vm) to restore an employee
-      // TODO : initialize controller (vm) by calling requestEmployees
+      // TODO : capture a reference to this controller with a variable
+      // TODO : implement a function on controller to request the list of employees
+      // TODO : implement a function on controller to remove an employee
+      // TODO : implement a function on controller to restore an employee
+      // TODO : initialize controller by calling requestEmployees
       
     }
   );
